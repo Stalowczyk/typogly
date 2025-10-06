ea9e90f90c64519179078507defcb3fd1f8ec355
Stalowczyk <pawel.stalowczyk@gmail.com>
2025-10-06 23:54:24 +0200

    Fix module exports (ESM/CJS) and rebuild dist

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  format: ['cjs', 'esm'],
   entry: ['./src/index.ts'],
+  format: ['cjs', 'esm'],     
   dts: true,
+  clean: true,
   shims: true,
   skipNodeModulesBundle: true,
-  clean: true,
+  outDir: 'dist',
+  esbuildOptions(options, ctx) {
+    if (ctx.format === 'esm') {
+      options.outExtension = { '.js': '.mjs' };
+    } else if (ctx.format === 'cjs') {
+      options.outExtension = { '.js': '.cjs.js' };
+    }
+  }
 });
