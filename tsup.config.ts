e562329aedcf4ad44f1965c91160a8a85b0f7fb0
Stalowczyk <pawel.stalowczyk@gmail.com>
2025-10-07 00:14:04 +0200

    Fix dual ESM/CJS exports and type declarations

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['./src/index.ts'],
-  format: ['cjs', 'esm'],     
-  dts: true,
-  clean: true,
+  format: ['cjs', 'esm'],      
+  dts: true,                   
+  clean: true,                  
   shims: true,
   skipNodeModulesBundle: true,
   outDir: 'dist',
   esbuildOptions(options, ctx) {
     if (ctx.format === 'esm') {
-      options.outExtension = { '.js': '.mjs' };
+      options.outExtension = { '.js': '.mjs' };   
     } else if (ctx.format === 'cjs') {
-      options.outExtension = { '.js': '.cjs.js' };
+      options.outExtension = { '.js': '.cjs' };   
     }
-  }
+  },
 });
